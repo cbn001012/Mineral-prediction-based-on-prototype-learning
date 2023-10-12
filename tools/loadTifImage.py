d749b79cf55c52ddd45498bd439441b4973fa7b7
cbn001012 <76193747+cbn001012@users.noreply.github.com>
2023-10-12 14:43:02 +0800

    Add files via upload

--- tools/loadTifImage.py
+++ tools/loadTifImage.py
@@ -8,21 +8,20 @@ from skimage import transform,io
 IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', 'webp']
 
 def has_file_allowed_extension(filename, extensions):
-    """查看文件是否是支持的可扩展类型
-
+    """
     Args:
-        filename (string): 文件路径
-        extensions (iterable of strings): 可扩展类型列表，即能接受的图像文件类型
+        filename (string)
+        extensions (iterable of strings)
 
     Returns:
         bool: True if the filename ends with one of given extensions
     """
     filename_lower = filename.lower()
-    return any(filename_lower.endswith(ext) for ext in extensions) # 返回True或False列表
+    return any(filename_lower.endswith(ext) for ext in extensions)
 
 def make_dataset(dir, class_to_idx, extensions):
     """
-        返回形如[(图像路径, 该图像对应的类别索引值),(),...]
+        Return in the form [(image path, corresponding class index), (), ...].
     """
     images = []
     dir = os.path.expanduser(dir)
@@ -31,9 +30,9 @@ def make_dataset(dir, class_to_idx, extensions):
         if not os.path.isdir(d):
             continue
 
-        for root, _, fnames in sorted(os.walk(d)): #层层遍历文件夹，返回当前文件夹路径，存在的所有文件夹名，存在的所有文件名
+        for root, _, fnames in sorted(os.walk(d)):
             for fname in sorted(fnames):
-                if has_file_allowed_extension(fname, extensions): #查看文件是否是支持的可扩展类型，是则继续
+                if has_file_allowed_extension(fname, extensions):
                     path = os.path.join(root, fname)
                     item = (path, class_to_idx[target])
                     images.append(item)
@@ -42,33 +41,30 @@ def make_dataset(dir, class_to_idx, extensions):
 
 def loadTifImage(path):
     image = io.imread(path)
-    # print('image.shape=>',image.shape)
-    image = transform.resize(image, (20, 20))     # 修改尺寸，仅能在此处修改 原224*224
-    image = image/255.0             # 归一化
-    # print(image)
+    image = transform.resize(image, (20, 20))
+    image = image/255.0
     im = np.array(image, dtype=np.float32)
     return im
 
 class DatasetFolder(Dataset):
     """
      Args:
-        root (string): 根目录路径
-        loader (callable): 根据给定的路径来加载样本的可调用函数
-        extensions (list[string]): 可扩展类型列表，即能接受的图像文件类型.
-        transform (callable, optional): 用于样本的transform函数，然后返回样本transform后的版本
-            E.g, ``transforms.RandomCrop`` for images.
-        target_transform (callable, optional): 用于样本标签的transform函数
+        root (string): Root directory path.
+        loader (callable): A callable function to load a sample given its path.
+        extensions (list[string]): A list of acceptable file extensions for images.
+        transform (callable, optional): A function that applies transformations to the samples and returns the transformed version.
+        E.g., transforms.RandomCrop for images.
+        target_transform (callable, optional): A function that applies transformations to the sample labels.
 
      Attributes:
-        classes (list): 类别名列表
-        class_to_idx (dict): 项目(class_name, class_index)字典,如{'cat': 0, 'dog': 1}
-        samples (list): (sample path, class_index) 元组列表，即(样本路径, 类别索引)
-        targets (list): 在数据集中每张图片的类索引值，为列表
+        classes (list): List of class names.
+        class_to_idx (dict): A dictionary mapping class names to class indices, e.g., {'cat': 0, 'dog': 1}.
+        samples (list): List of (sample path, class index) tuples, where each tuple represents the path of a sample and its corresponding class index.
+        targets (list): List of class indices for each image in the dataset.
     """
 
     def __init__(self, root, loader=loadTifImage, extensions=IMG_EXTENSIONS, transform=None, target_transform=None):
-        classes, class_to_idx = self._find_classes(root)    # 得到类名和类索引，如['cat', 'dog']和{'cat': 0, 'dog': 1}
-        # 返回形如[(图像路径, 该图像对应的类别索引值),(),...]，即对每个图像进行标记
+        classes, class_to_idx = self._find_classes(root)
         samples = make_dataset(root, class_to_idx, extensions)
         if len(samples) == 0:
             raise(RuntimeError("Found 0 files in subfolders of: " + root + "\n"
@@ -81,32 +77,20 @@ class DatasetFolder(Dataset):
         self.classes = classes
         self.class_to_idx = class_to_idx
         self.samples = samples
-        self.targets = [s[1] for s in samples]  # 所有图像的类索引值组成的列表
+        self.targets = [s[1] for s in samples]
 
         self.transform = transform
         self.target_transform = target_transform
 
     def _find_classes(self, dir):
-        """
-        在数据集中查找类文件夹。
-
-        Args:
-            dir (string): 根目录路径
-
-        Returns:
-            返回元组: (classes, class_to_idx)即(类名, 类索引)，其中classes即相应的目录名，如['cat', 'dog'];class_to_idx为形如{类名:类索引}的字典，如{'cat': 0, 'dog': 1}.
-
-        Ensures:
-            保证没有类名是另一个类目录的子目录
-        """
         if sys.version_info >= (3, 5):
             # Faster and available in Python 3.5 and above
-            classes = [d.name for d in os.scandir(dir) if d.is_dir()]   # 获得根目录dir的所有第一层子目录名
+            classes = [d.name for d in os.scandir(dir) if d.is_dir()]
         else:
-            classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]   # 效果和上面的一样，只是版本不同方法不同
-        classes.sort() #然后对类名进行排序
-        class_to_idx = {classes[i]: i for i in range(len(classes))}     # 然后将类名和索引值一一对应的到相应字典，如{'cat': 0, 'dog': 1}
-        return classes, class_to_idx    # 然后返回类名和类索引
+            classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
+        classes.sort()
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        return classes, class_to_idx
 
     def __getitem__(self, index):
         """
@@ -117,7 +101,7 @@ class DatasetFolder(Dataset):
             tuple: (sample, target) where target is class_index of the target class.
         """
         path, target = self.samples[index]
-        sample = self.loader(path)  # 加载图片函数，可自定义为opencv，默认为PIL
+        sample = self.loader(path)
         if self.transform is not None:
             sample = self.transform(sample)
         if self.target_transform is not None:
--- tools/train_val_test_split.py
+++ tools/train_val_test_split.py
@@ -1,4 +1,3 @@
-# 工具类
 import os
 import random
 import shutil
@@ -7,17 +6,17 @@ from shutil import copy2
 
 def data_set_split(src_data_folder, target_data_folder, train_scale=0.7, val_scale=0, test_scale=0.3):
     '''
-    读取源数据文件夹，生成划分好的文件夹，分为trian、val、test三个文件夹进行
-    :param src_data_folder: 源文件夹 E:/biye/gogogo/note_book/torch_note/data/utils_test/data_split/src_data
-    :param target_data_folder: 目标文件夹 E:/biye/gogogo/note_book/torch_note/data/utils_test/data_split/target_data
-    :param train_scale: 训练集比例
-    :param val_scale: 验证集比例
-    :param test_scale: 测试集比例
+    Read the source data folder and generate a split folder with 'train', 'val', and 'test' subfolders.
+    :param src_data_folder: Source folder E:/biye/gogogo/note_book/torch_note/data/utils_test/data_split/src_data
+    :param target_data_folder: Target folder E:/biye/gogogo/note_book/torch_note/data/utils_test/data_split/target_data
+    :param train_scale: Proportion of training set
+    :param val_scale: Proportion of validation set
+    :param test_scale: Proportion of test set
     :return:
     '''
-    print("开始数据集划分")
+    print("Start data set splitting")
     class_names = os.listdir(src_data_folder)
-    # 在目标目录下创建文件夹
+    # Create folders in the target directory
     split_names = ['train', 'val', 'test']
     for split_name in split_names:
         split_path = os.path.join(target_data_folder, split_name)
@@ -25,7 +24,7 @@ def data_set_split(src_data_folder, target_data_folder, train_scale=0.7, val_sca
             pass
         else:
             os.mkdir(split_path)
-        # 然后在split_path的目录下创建类别文件夹
+        # Create class folders under split_path directory
         for class_name in class_names:
             class_split_path = os.path.join(split_path, class_name)
             if os.path.isdir(class_split_path):
@@ -33,8 +32,8 @@ def data_set_split(src_data_folder, target_data_folder, train_scale=0.7, val_sca
             else:
                 os.mkdir(class_split_path)
 
-    # 按照比例划分数据集，并进行数据图片的复制
-    # 首先进行分类遍历
+    # Split the dataset and copy data images according to the proportions
+    # Traverse through the classes
     for class_name in class_names:
         current_class_data_path = os.path.join(src_data_folder, class_name)
         current_all_data = os.listdir(current_class_data_path)
@@ -55,29 +54,28 @@ def data_set_split(src_data_folder, target_data_folder, train_scale=0.7, val_sca
             src_img_path = os.path.join(current_class_data_path, current_all_data[i])
             if current_idx <= train_stop_flag:
                 copy2(src_img_path, train_folder)
-                # print("{}复制到了{}".format(src_img_path, train_folder))
+                # print("{} copied to {}".format(src_img_path, train_folder))
                 train_num = train_num + 1
             elif (current_idx > train_stop_flag) and (current_idx <= val_stop_flag):
                 copy2(src_img_path, val_folder)
-                # print("{}复制到了{}".format(src_img_path, val_folder))
+                # print("{} copied to {}".format(src_img_path, val_folder))
                 val_num = val_num + 1
             else:
                 copy2(src_img_path, test_folder)
-                # print("{}复制到了{}".format(src_img_path, test_folder))
+                # print("{} copied to {}".format(src_img_path, test_folder))
                 test_num = test_num + 1
 
             current_idx = current_idx + 1
 
         print("*********************************{}*************************************".format(class_name))
-        print(
-            "{}类按照{}：{}：{}的比例划分完成，一共{}张图片".format(class_name, train_scale, val_scale, test_scale, current_data_length))
-        print("训练集{}：{}张".format(train_folder, train_num))
-        print("验证集{}：{}张".format(val_folder, val_num))
-        print("测试集{}：{}张".format(test_folder, test_num))
+        print("{} class split completed with a ratio of {}:{}:{}, a total of {} images".format(class_name, train_scale, val_scale, test_scale, current_data_length))
+        print("Training set {}: {} images".format(train_folder, train_num))
+        print("Validation set {}: {} images".format(val_folder, val_num))
+        print("Test set {}: {} images".format(test_folder, test_num))
 
 
 if __name__ == '__main__':
     src_data_folder = "..."
     target_data_folder = "..."
-    os.makedirs(target_data_folder,exist_ok=True)
+    os.makedirs(target_data_folder, exist_ok=True)
     data_set_split(src_data_folder, target_data_folder)
